412fb73a1d0c8cc63a8e4b38b243f78b675bee25
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-12-01 20:40:26 +0500

    Login and Register Page Updated

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,21 @@
+import React from 'react'
+import LoginPage from './Components/LoginPage/LoginPage'
+import RegisterPage from './Components/RegisterPage/RegisterPage';
+import {BrowserRouter as Router,Route,Routes,Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Routes>
+
+       <Route exact path='/' element={<RegisterPage />} />
+       <Route exact path='/register' element={<RegisterPage />} />
+       <Route exact path='/login' element={<LoginPage />} />
+
+      </Routes>
+    </Router>
+   
+    
   );
 }
 
