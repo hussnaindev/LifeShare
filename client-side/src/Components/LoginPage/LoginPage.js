137f0a822e3ac0345be7030d4c586938d5af5758
MeerHamza1421 <hamzababar37@gmail.com>
2021-12-04 14:34:21 -0800

    ♻ Refractor: Login and Registeration Code

--- client-side/src/Components/LoginPage/LoginPage.js
+++ client-side/src/Components/LoginPage/LoginPage.js
@@ -1,73 +1,64 @@
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import { Button} from '@mui/material';
-import { InputAdornment } from '@material-ui/core';
-import { Visibility,VisibilityOff } from '@material-ui/icons';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LockIcon from '@mui/icons-material/Lock';
-import IconButton from '@mui/material/IconButton';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import { IconButton, InputAdornment, TextField, Button } from '@material-ui/core';
+import { LockRounded, AccountCircleRounded, Visibility, VisibilityOff } from '@material-ui/icons';
+import { Link } from 'react-router-dom'
 import './LoginPage.css'
 
-const LoginPage = () =>
-{
+const LoginPage = () => {
 
-    const [username,setUsername] = useState('')
-    const [password,setPassword] = useState('')
-    const [showPassword,setShowPassword] = useState(false)
-    
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
 
-    const handleUsername = (e) =>
-    {
+    const handleUsername = (e) => {
         console.log(e.target.value)
         setUsername(e.target.value)
 
     }
 
-    const handlePassword = (e) =>
-    {
+    const handlePassword = (e) => {
         console.log(e.target.value)
         setPassword(e.target.value)
     }
 
-    const handleVisibilty = () =>
-    {
+    const handleVisibilty = () => {
         setShowPassword(!showPassword)
     }
 
-    const handleLoginForm = (e) =>
-    {
+    const handleLoginForm = (e) => {
         e.preventDefault()
     }
 
-
-    return(
+    return (
         <div className='loginPage'>
 
             <div className='loginForm-container'>
 
                 <form className='loginForm' onSubmit={handleLoginForm}>
-                    <AccountCircleIcon style={{fontSize:60}}/>
+                    <AccountCircleRounded style={{ fontSize: 60 }} />
 
                     <div className='username-container'>
                         <TextField className='username' label="Username" variant="outlined" value={username} onChange={handleUsername}
-                         InputProps={{ startAdornment: <InputAdornment position="start">
-                             <AccountCircleIcon />
-                         </InputAdornment>}}/>
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">
+                                    <AccountCircleRounded />
+                                </InputAdornment>
+                            }} />
                     </div>
 
                     <div className='password-container'>
-                         <TextField className='password' label="Password" type={showPassword ? 'text' : 'password'} variant="outlined" value={password} onChange={handlePassword}
-                           InputProps={{ endAdornment: <InputAdornment position="end">
+                        <TextField className='password' label="Password" type={showPassword ? 'text' : 'password'} variant="outlined" value={password} onChange={handlePassword}
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">
                                     <IconButton className='iconbtn' onClick={handleVisibilty}>
-                                        {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
                                     </IconButton>
                                 </InputAdornment>,
                                 startAdornment: <InputAdornment position="start">
-                                <LockIcon />
-                                 </InputAdornment>
-                                }} 
-                         />             
+                                    <LockRounded />
+                                </InputAdornment>
+                            }}
+                        />
                     </div>
 
                     <div className='forgot-pwd-container'>
@@ -75,14 +66,14 @@ const LoginPage = () =>
                     </div>
 
                     <div className='login-btn-container'>
-                        <Button className='login-btn' variant='contained' type='submit'>Login</Button>
+                        <Button variant='contained' color="primary" className="login-btn" type='submit'>Login</Button>
                     </div>
 
                     <div className='link-to-register-container'>
-                      Don't have account?
-                      <Link className='link-to-register' to='/register'>Register</Link>   
+                        Don't have account?
+                        <Link className='link-to-register' to='/register'>Register</Link>
                     </div>
-                   
+
                 </form>
             </div>
         </div>
--- client-side/src/Components/RegisterPage/RegisterPage.js
+++ client-side/src/Components/RegisterPage/RegisterPage.js
@@ -1,130 +1,122 @@
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import { Button} from '@mui/material';
-import { InputAdornment } from '@material-ui/core';
-import { Visibility,VisibilityOff } from '@material-ui/icons';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LockIcon from '@mui/icons-material/Lock';
-import EmailIcon from '@mui/icons-material/Email';
-import IconButton from '@mui/material/IconButton';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import { IconButton, InputAdornment, TextField, Button } from '@material-ui/core';
+import { EmailRounded, LockRounded, AccountCircleRounded, Visibility, VisibilityOff } from '@material-ui/icons';
+import { Link } from 'react-router-dom'
 import './RegisterPage.css'
 
-const RegisterPage = () =>
-{
+const RegisterPage = () => {
 
-    const [username,setUsername] = useState('')
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    const [confirmPassword,setConfirmPassword] = useState('')
-    const [showPassword,setShowPassword] = useState(false)
-    const [showConfirmPassword,setShowConfirmPassword] = useState(false)
-    
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
-    const handleUsername = (e) =>
-    {
+
+    const handleUsername = (e) => {
         console.log(e.target.value)
         setUsername(e.target.value)
 
     }
 
-    const handleEmail = (e) =>
-    {
+    const handleEmail = (e) => {
         console.log(e.target.value)
         setEmail(e.target.value)
     }
 
-    const handlePassword = (e) =>
-    {
+    const handlePassword = (e) => {
         console.log(e.target.value)
         setPassword(e.target.value)
     }
 
-    const handleConfirmPassword = (e) =>
-    {
+    const handleConfirmPassword = (e) => {
         console.log(e.target.value)
         setConfirmPassword(e.target.value)
     }
 
-    const handleVisibilty = (input) =>
-    {
-        if(input==='password')
-        {
+    const handleVisibilty = (input) => {
+        if (input === 'password') {
             setShowPassword(!showPassword)
         }
 
-        else if(input==='confirmPassword')
-        {
+        else if (input === 'confirmPassword') {
             setShowConfirmPassword(!showConfirmPassword)
-        } 
+        }
     }
 
-    const handleRegisterForm = (e) =>
-    {
+    const handleRegisterForm = (e) => {
         e.preventDefault()
     }
 
 
-    return(
+    return (
         <div className='registerPage'>
 
             <div className='registerForm-container'>
 
                 <form className='registerForm' onSubmit={handleRegisterForm}>
-                    <AccountCircleIcon style={{fontSize:60}}/>
+                    <AccountCircleRounded style={{ fontSize: 60 }} />
 
                     <div className='email-container'>
                         <TextField className='email' label="Email" variant="outlined" value={email} onChange={handleEmail}
-                         InputProps={{ startAdornment: <InputAdornment position="start">
-                             <EmailIcon />
-                         </InputAdornment>}}/>
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">
+                                    <EmailRounded />
+                                </InputAdornment>
+                            }}
+                        />
                     </div>
 
                     <div className='username-container'>
                         <TextField className='username' label="Username" variant="outlined" value={username} onChange={handleUsername}
-                         InputProps={{ startAdornment: <InputAdornment position="start">
-                             <AccountCircleIcon />
-                         </InputAdornment>}}/>
+                            InputProps={{
+                                startAdornment: <InputAdornment position="start">
+                                    <AccountCircleRounded />
+                                </InputAdornment>
+                            }} />
                     </div>
 
                     <div className='password-container'>
-                         <TextField className='password' label="Password" type={showPassword ? 'text' : 'password'} variant="outlined" value={password} onChange={handlePassword}
-                           InputProps={{ endAdornment: <InputAdornment position="end">
+                        <TextField className='password' label="Password" type={showPassword ? 'text' : 'password'} variant="outlined" value={password} onChange={handlePassword}
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">
                                     <IconButton className='iconbtn' onClick={() => handleVisibilty('password')}>
-                                        {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
                                     </IconButton>
                                 </InputAdornment>,
                                 startAdornment: <InputAdornment position="start">
-                                <LockIcon />
-                                 </InputAdornment>
-                                }} 
-                         />
+                                    <LockRounded />
+                                </InputAdornment>
+                            }}
+                        />
                     </div>
 
-                         
+
                     <div className='confirmPassword-container'>
-                         <TextField className='confirmPassword' label="Confirm Password" type={showConfirmPassword ? 'text' : 'password'} variant="outlined" value={confirmPassword} onChange={handleConfirmPassword}
-                           InputProps={{ endAdornment: <InputAdornment position="end">
+                        <TextField className='confirmPassword' label="Confirm Password" type={showConfirmPassword ? 'text' : 'password'} variant="outlined" value={confirmPassword} onChange={handleConfirmPassword}
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">
                                     <IconButton className='iconbtn' onClick={() => handleVisibilty('confirmPassword')}>
-                                        {showConfirmPassword ? <Visibility/> : <VisibilityOff/>}
+                                        {showConfirmPassword ? <Visibility /> : <VisibilityOff />}
                                     </IconButton>
                                 </InputAdornment>,
                                 startAdornment: <InputAdornment position="start">
-                                <LockIcon />
-                                 </InputAdornment>
-                                }} 
-                         />
-                                      
+                                    <LockRounded />
+                                </InputAdornment>
+                            }}
+                        />
+
                     </div>
 
                     <div className='register-btn-container'>
-                        <Button className='register-btn' variant='contained' type='submit'>Register</Button>
+                        <Button className='register-btn' color="primary" variant='contained' type='submit'>Register</Button>
                     </div>
 
-                   <div className='link-to-login-container'>
-                       <p>Already have account?<Link className='link-to-login' to='/login' >Login</Link></p>
-                   </div>
-                   
+                    <div className='link-to-login-container'>
+                        <p>Already have account?<Link className='link-to-login' to='/login' >Login</Link></p>
+                    </div>
+
                 </form>
             </div>
         </div>
