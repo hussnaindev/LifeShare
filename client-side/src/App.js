540c295ba985446809cd8c8850244994683e4aac
MeerHamza1421 <hamzababar37@gmail.com>
2021-12-09 09:04:03 -0800

    🐛 FIX: Merge Conflicts

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import "./App.css";
-import Home from './Components/Home/JavaScript/Home';
+import Home from './Components/Home/Home';
 import LoginPage from './Components/LoginPage/JavaScript/LoginPage';
 import RegisterPage from './Components/RegisterPage/JavaScript/RegisterPage';
-
+import MapService from './Components/Map/MapService';
 function App() {
     return (
       <Router>
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/register' element={<RegisterPage />} />
-          <Route exact path='/login' element={<LoginPage />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/locationservice' element={<MapService />} />
         </Routes>
       </Router>
     );
