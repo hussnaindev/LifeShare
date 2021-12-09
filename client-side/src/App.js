0126a94cb528489de4012b7b7c7aaa6f4c59ab35
MeerHamza1421 <hamzababar37@gmail.com>
2021-12-09 09:10:55 -0800

    🐛 FIX: File Paths

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import Home from './Components/Home/Home';
-import LoginPage from './Components/LoginPage/JavaScript/LoginPage';
-import RegisterPage from './Components/RegisterPage/JavaScript/RegisterPage';
+import LoginPage from './Components/LoginPage/LoginPage';
+import RegisterPage from './Components/RegisterPage/RegisterPage';
 import MapService from './Components/Map/MapService';
+
 function App() {
-    return (
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/locationservice' element={<MapService />} />
-        </Routes>
-      </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/locationservice' element={<MapService />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
